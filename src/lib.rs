f26563973d34accb930c89592908cdff7e507d8a
DashaPoliazz <dasha.poliazzz@gmail.com>
2024-03-01 16:52:55 +0200

    Init

--- src/lib.rs
+++ src/lib.rs
@@ -7,18 +7,4 @@ extern crate wee_alloc;
 #[global_allocator]
 static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    unsafe {
-        alert(name);
-    }
-}
-
-#[wasm_bindgen]
-// Imlementation of this fn will be provided by
-// javascript
-extern "C" {
-    pub fn alert(s: &str);
-}
-
 // wasm-pack build --target web
